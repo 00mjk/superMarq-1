63171275f19013fd7ba01d10be654fb2cb24738b
Teague Tomesh <ttomesh@princeton.edu>
2021-07-19 17:48:45 -0500

    Test ghz benchmark

---
+++ supermarq/benchmarks/ghz_test.py
@@ -0,0 +1,16 @@
+import collections
+
+import cirq
+
+from supermarq.benchmarks.ghz import GHZ
+
+
+def test_ghz_circuit() -> None:
+    ghz = GHZ(3)
+    assert len(ghz.circuit().all_qubits()) == 3
+
+
+def test_ghz_score() -> None:
+    ghz = GHZ(3)
+    assert ghz.score({'000':500, '111':500}) == 1
+
