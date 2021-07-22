5068b6c53d87c2748ec2576550f0a580a9fee438
Teague Tomesh <ttomesh@princeton.edu>
2021-07-22 12:51:44 -0500

    Implement VQE proxy benchmark.

--- supermarq/__init__.py
+++ supermarq/__init__.py
@@ -1,5 +1,6 @@
 from .benchmarks import benchmark
 from .benchmarks import ghz
 from .benchmarks import hamiltonian_simulation
+from .benchmarks import vqe_proxy
 
-__all__ = ["benchmark", "ghz", "hamiltonian_simulation"]
+__all__ = ["benchmark", "ghz", "hamiltonian_simulation", "vqe_proxy"]
