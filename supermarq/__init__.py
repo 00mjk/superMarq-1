24065caff4faa5e7e0577353748b9f11d7582010
Teague Tomesh <ttomesh@princeton.edu>
2021-07-22 15:42:03 -0500

    Implement bit code benchmark

--- supermarq/__init__.py
+++ supermarq/__init__.py
@@ -1,5 +1,6 @@
 from .benchmarks import benchmark
+from .benchmarks import bit_code
 from .benchmarks import ghz
 from .benchmarks import hamiltonian_simulation
 
-__all__ = ["benchmark", "ghz", "hamiltonian_simulation"]
+__all__ = ["benchmark", "ghz", "hamiltonian_simulation", "bit_code"]
