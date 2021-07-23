4c796a9cd92fc8dd879cea3af9976d8c5e485bee
vtomole <vtomole2@gmail.com>
2021-07-23 10:49:34 -0500

    Add aws ro run_ghz_example

--- examples/ghz_example.py
+++ examples/ghz_example.py
@@ -7,14 +7,13 @@ import time
 
 def main():
     service = cirq_superstaq.Service(
-            remote_host="https://127.0.0.1:5000",
-            api_key="""Paste your SuperstaQ token here""",
-            ibmq_token="Paste your IBM Token here",
-            ibmq_hub="ibm-q-startup",
-            ibmq_group="super-tech-labs",
-            ibmq_project="default",
-            ibmq_pulse='false',
-            verbose=True,
+        remote_host="https://127.0.0.1:5000",
+        api_key="""Paste your SuperstaQ token here""",
+        ibmq_token="Paste your IBM Token here",
+        ibmq_hub="ibm-q-startup",
+        ibmq_group="super-tech-labs",
+        ibmq_project="default",
+        verbose=True,
     )
 
 
@@ -23,18 +22,33 @@ def main():
 
     print(ghz.circuit())
 
-    job = service.create_job(circuit=ghz.circuit(), repetitions=1000, target="ibmq_qasm_simulator")
-    print("Created job:", job)
-    print("Current status:", job.status())
+    ibm_job = service.create_job(circuit=ghz.circuit(), repetitions=100, target="ibmq_qasm_simulator")
+    print("Created IBM job:", ibm_job)
+    print("Current IBM status:", ibm_job.status())
 
-    while job.status() != "Done":
-        time.sleep(5)
+    while ibm_job.status() != "Done":
+        time.sleep(10)
 
-    print("Job status:", job.status())
-    print('Counts:', job.counts())
-    print("Benchmark score:", ghz.score(job.counts()))
-    print('Job_id:', job.job_id())
-    print("Service.get_job:",service.get_job(job.job_id()))
+    print("IBM Job status:", ibm_job.status())
+    print('IBM Counts:', ibm_job.counts())
+    print("IBM Benchmark score:", ghz.score(ibm_job.counts()))
+    print('IBM Job_id:', ibm_job.job_id())
+    ibm_job = service.get_job("60fade94d13f733f24d134fc")
+    print('IBM Counts:', ibm_job.counts())
+
+
+    aws_job = service.create_job(circuit=ghz.circuit(), repetitions=100, target="aws_sv1_simulator")
+    print("Created AWS job:", aws_job)
+    print("Current AWS status:", aws_job.status())
+
+    while aws_job.status() != "Done":
+        time.sleep(10)
+
+    print("AWS Job status:", aws_job.status())
+    print('AWS Counts:', aws_job.counts())
+    print("AWS Benchmark score:", ghz.score(aws_job.counts()))
+    print('AWS Job_id:', aws_job.job_id())
+    print("AWS Service.get_job:",service.get_job(aws_job.job_id()))
 
 
 if __name__=='__main__':
